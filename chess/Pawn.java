f935886a638728797e373054a781144740baabed
GuBorui <36877128+GuBorui@users.noreply.github.com>
2018-03-07 20:40:28 +0800

    Update Pawn.java

--- chess/Pawn.java
+++ chess/Pawn.java
@@ -1,26 +1,11 @@
 public class Pawn extends Pieces
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Pawn
-     */
-    public Pawn()
+    Pawn(String colour)
     {
-        // initialise instance variables
-        x = 0;
+        super.colour=colour;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public String toString()
     {
-        // put your code here
-        return x + y;
+        return colour+"P";
     }
 }
