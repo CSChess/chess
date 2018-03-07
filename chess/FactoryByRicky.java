0d7e41ceb06f505e06ec51889a47b751fe20a9c8
GuBorui <36877128+GuBorui@users.noreply.github.com>
2018-03-07 21:29:40 +0800

    wrong code fixing

--- chess/FactoryByRicky.java
+++ chess/FactoryByRicky.java
@@ -12,42 +12,42 @@ public class FactoryByRicky
     }
     public Knight creatN(){
         if(numOfN == 2){
-            new Error("Out of number");
+            throw new Error("Out of number");
         }
         numOfN++;
         return new Knight(colour);
     }
     public Bishop creatB(){
         if(numOfB == 2){
-            new Error("Out of number");
+           throw new Error("Out of number");
         }
         numOfB++;
         return new Bishop(colour);
     }
     public Pawn creatP(){
         if(numOfP == 8){
-            new Error("Out of number");
+            throw new Error("Out of number");
         }
         numOfP++;
         return new Pawn(colour);
     }
     public Queen creatQ(){
         if(numOfQ >0){
-            new Error("Out of number");
+            throw new Error("Out of number");
         }
         numOfQ++;
         return new Queen(colour);
     }
     public King creatK(){
         if(numOfK > 0){
-            new Error("Out of number");
+            throw new Error("Out of number");
         }
         numOfK++;
         return new King(colour);
     }
     public Rook creatR(){
         if(numOfR == 2){
-            new Error("Out of number");
+            throw new Error("Out of number");
         }
         numOfR++;
         return new Rook(colour);
