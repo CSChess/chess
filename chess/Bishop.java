280a2d9b72f6e718fc34faf997c7cb33da2a82c3
GuBorui <36877128+GuBorui@users.noreply.github.com>
2018-03-07 20:40:01 +0800

    Update Bishop.java

--- chess/Bishop.java
+++ chess/Bishop.java
@@ -1,4 +1,11 @@
 public class Bishop extends Pieces
 {
-    
+    Bishop(String colour)
+    {
+       super.colour=colour;
+    }
+    public String toString()
+    {
+        return colour+"B";
+    }
 }
