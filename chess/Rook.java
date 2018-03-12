98233c739cc3161ac6788a96efdd4e1271b1b205
Hunter-19 <m18518368361@163.com>
2018-03-12 12:12:46 +0800

    gygyg

--- chess/Rook.java
+++ chess/Rook.java
@@ -3,17 +3,17 @@ public class Rook extends Pieces
     String colour;
     Rook(String colour)
     {
-<<<<<<< HEAD
+
        if(color=="B")
        {return "Rook(B)";}
        else
        {return "Rook(W)";}
-=======
+
        this.colour=colour;
     }
     public String toString()
     {
         return colour+"R";
->>>>>>> 9bcf998bdb0c1db9970bf4232b880bacce7e25cc
+
     }
 }
