2d7bd1e413c0db782390a4e81b6c93c8770e4dfb
dsxsteven <36877507+dsxsteven@users.noreply.github.com>
2018-03-30 13:01:01 +0800

    Debug Rook

--- Rook.java
+++ Rook.java
@@ -9,19 +9,18 @@ public class Rook extends Pieces
         int rowF = f.getRow();
         int colF = f.getCol();
        
-        
         if(!(rowI==rowF||colI==colF))throw new WrongMoveException("");
         
         if(rowI==rowF){
             int a = (int)((colF-colI)/(Math.abs(colF-colI)));
-            for(int x = colI+a;x<colF;x+=a){
-                if(board[rowI][x]!=null)throw new WrongMoveException("");
+            for(int x = colI+a;x!=colF;x+=a){
+                if(board[rowI][x].getPieces()!=null)throw new WrongMoveException("");
             }
         }
         else if(colI==colF){
             int a =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
-            for(int x = rowI+1;x<rowF;x+=a){
-                if(board[x][colI]!=null)throw new WrongMoveException("");
+            for(int x = rowI+a;x!=rowF;x+=a){
+                if(board[x][colI].getPieces()!=null)throw new WrongMoveException("");
             }
         }
         return;
