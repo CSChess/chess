5f6db4e556cc524fe4479b68d3dce58444519f24
z11GWYSN <36877388+z11GWYSN@users.noreply.github.com>
2018-04-02 22:07:13 +0800

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,5 +1,6 @@
 public class Bishop extends Pieces
 {
+    Bishop(){}
     Bishop(boolean colour){
         super(colour);
     }
@@ -33,6 +34,31 @@ public class Bishop extends Pieces
            if(board[x][y].getPieces()!=null)throw new WrongMoveException(""); 
         }
         return;
+    }
+    boolean judgeKing(Grid i,Grid f,Grid[][] board){
+        int rowI = i.getRow();
+        int colI = i.getCol();
+        int rowF = f.getRow();
+        int colF = f.getCol();
+        int row = rowF-rowI;
+        int col = colF-colI;
+        boolean checkmate=false;
+        if(Math.abs(row)!=Math.abs(col))return checkmate;
+        int a;
+        int b;
+        if(row>0)a=1;
+        else a=-1;
+        if(col>0)b=1;
+        else b=-1;
+        int x=rowI;
+        int y=colI;
+        while(x!=(rowF-a) && y!=(colF-b))
+        {
+           x=x+a;
+           y=y+b;
+           if(board[x][y].getPieces()!=null)return checkmate; 
+        }
+        return true;
     }
     public String toString(){
         if(super.getColour()==true)return "WB";
