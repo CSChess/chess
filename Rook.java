8f6a050044fc336b76b9b84ad0706f9bcd9c90c0
z11GWYSN <36877388+z11GWYSN@users.noreply.github.com>
2018-04-02 21:57:11 +0800

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,5 +1,6 @@
 public class Rook extends Pieces
 {
+    Rook(){}
     Rook(boolean colour){
         super(colour);
     }
@@ -24,6 +25,27 @@ public class Rook extends Pieces
             }
         }
         return;
+    }
+    boolean judgeKing(Grid i,Grid f,Grid[][] board){
+        int rowI = i.getRow();
+        int colI = i.getCol();
+        int rowF = f.getRow();
+        int colF = f.getCol();
+        boolean checkmate=false;
+        if(!(rowI==rowF||colI==colF))return checkmate;
+        if(rowI==rowF){
+            int a = (int)((colF-colI)/(Math.abs(colF-colI)));
+            for(int x = colI+a;x!=colF;x+=a){
+                if(board[rowI][x].getPieces()!=null)return checkmate;
+            }
+        }
+        else if(colI==colF){
+            int a =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
+            for(int x = rowI+a;x!=rowF;x+=a){
+                if(board[x][colI].getPieces()!=null)return checkmate;
+            }
+        }
+        return true;
     }
     public String toString(){
         if(super.getColour()==true)return "WR";
