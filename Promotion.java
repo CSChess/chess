2d8053e7afd782260aa67fa6d6e8096e13fef7f0
z11GWYSN <36877388+z11GWYSN@users.noreply.github.com>
2018-04-08 21:10:16 +0800

    Update Promotion.java

--- Promotion.java
+++ Promotion.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class RightInput here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class  Promotion
 { 
     static boolean  promotionCheck=false;
