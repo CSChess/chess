36e8fd138ad7ed1dba37e62b60c146f0a58793bd
dsxsteven <36877507+dsxsteven@users.noreply.github.com>
2018-04-23 19:37:00 +0800

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -4,42 +4,46 @@ public class Rook extends Pieces
     Rook(boolean colour){
         super(colour);
     }
-    void judge(Grid i,Grid f,Grid[][] board)throws WrongMoveException{
-        int rowI = i.getRow();
-        int colI = i.getCol();
-        int rowF = f.getRow();
-        int colF = f.getCol();
+    void judge(Grid initialGrid,Grid finalGrid,Grid[][] board)throws WrongMoveException{
+        int rowI = initialGrid.getRow();
+        int colI = initialGrid.getCol();
+        int rowF = finalGrid.getRow();
+        int colF = finalGrid.getCol();
+       
         if(!(rowI==rowF||colI==colF))throw new WrongMoveException("");
-        if(rowI==rowF){
-            int a = (int)((colF-colI)/(Math.abs(colF-colI)));
-            for(int x = colI+a;x!=colF;x+=a){
+        
+        if(rowI==rowF)
+        {
+            int stride = (int)((colF-colI)/(Math.abs(colF-colI)));
+            for(int x = colI+stride;x!=colF;x+=stride){
                 if(board[rowI][x].getPieces()!=null)throw new WrongMoveException("");
             }
         }
         else if(colI==colF){
-            int a =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
-            for(int x = rowI+a;x!=rowF;x+=a){
-                if(board[x][colI].getPieces()!=null)throw new WrongMoveException("");
+            int stride =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
+            for(int x = rowI+stride;x<rowF;x+=stride)
+            {
+                if(board[x][colI]!=null)throw new WrongMoveException("");
             }
+            return;
         }
-        return;
     }
-    boolean judgeKing(Grid i,Grid f,Grid[][] board){
-        int rowI = i.getRow();
-        int colI = i.getCol();
-        int rowF = f.getRow();
-        int colF = f.getCol();
+    boolean judgeKing(Grid initialGrid,Grid finalGrid,Grid[][] board){
+        int rowI = initialGrid.getRow();
+        int colI = initialGrid.getCol();
+        int rowF = finalGrid.getRow();
+        int colF = finalGrid.getCol();
         boolean checkmate=false;
         if(!(rowI==rowF||colI==colF))return checkmate;
         if(rowI==rowF){
-            int a = (int)((colF-colI)/(Math.abs(colF-colI)));
-            for(int x = colI+a;x!=colF;x+=a){
+            int stride = (int)((colF-colI)/(Math.abs(colF-colI)));
+            for(int x = colI+stride;x!=colF;x+=stride){
                 if(board[rowI][x].getPieces()!=null)return checkmate;
             }
         }
         else if(colI==colF){
-            int a =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
-            for(int x = rowI+a;x!=rowF;x+=a){
+            int stride =(int)((rowF-rowI)/(Math.abs(rowF-rowI)));
+            for(int x = rowI+stride;x!=rowF;x+=stride){
                 if(board[x][colI].getPieces()!=null)return checkmate;
             }
         }
