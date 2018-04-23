d87beafd8dd92481f0b36571e7b45c2c7cd5a827
dsxsteven <36877507+dsxsteven@users.noreply.github.com>
2018-04-23 19:47:03 +0800

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -4,17 +4,17 @@ public class Knight extends Pieces
     Knight(boolean colour){
         super(colour);
     }
-    void judge(Grid i,Grid f,Grid[][] board) throws WrongMoveException{
-        int row = Math.abs(i.getRow()-f.getRow());
-        int col = Math.abs(i.getCol()-f.getCol());
+    void judge(Grid initialGrid,Grid finalGrid,Grid[][] board) throws WrongMoveException{
+        int row = Math.abs(initialGrid.getRow()-finalGrid.getRow());
+        int col = Math.abs(initialGrid.getCol()-finalGrid.getCol());
         if(row*col==2&&row+col==3){
             return;
         }
         throw new WrongMoveException("");
     }
-    boolean judgeKing(Grid i,Grid f,Grid[][] board){
-        int row = Math.abs(i.getRow()-f.getRow());
-        int col = Math.abs(i.getCol()-f.getCol());
+    boolean judgeKing(Grid initialGrid,Grid finalGrid,Grid[][] board){
+        int row = Math.abs(initialGrid.getRow()-finalGrid.getRow());
+        int col = Math.abs(initialGrid.getCol()-finalGrid.getCol());
         boolean checkmate=false;
         if(row*col==2&&row+col==3){
             return true;
